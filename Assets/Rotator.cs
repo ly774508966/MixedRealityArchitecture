30dec0f4ed46a773a350285a44d4dc603e5a7c77
Ricardo Gomes <ricardo.gomes@heptasoft.com>
2017-04-02 16:10:09 +0200

    Final touches and updates

---
+++ Assets/Rotator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	public GameObject model;
+	public GameObject model1;
+	public GameObject model2;
+	public GameObject model3;
+	public GameObject model4;
+	public GameObject model5;
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(0.5f);
+		model.SetActive (true);
+		yield return new WaitForSeconds(0.5f);
+		model1.SetActive (true);
+		yield return new WaitForSeconds(0.5f);
+		model2.SetActive (true);
+		yield return new WaitForSeconds(0.5f);
+		model3.SetActive (true);
+		yield return new WaitForSeconds(0.5f);
+		model4.SetActive (true);
+		yield return new WaitForSeconds(0.5f);
+		model5.SetActive (true);
+		this.GetComponentInParent<Canvas>().enabled=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (new Vector3(0,0,-5));
+
+	}
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-Mixed-Reality project developed and presented on AEC Hackathon, 31st of March to 2nd of April @ Tum - Munich 
-
-More information in https://wiki.munichmakerlab.de/wiki/Hackathons and http://aechackathon.com/munich/
